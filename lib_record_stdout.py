c434245be1761d54efd4796048075407ea1abcaa
BM 1 <infobertrand@free.fr>
2016-09-02 08:05:02 +0200

    avancé réparation décorateur

--- lib_record_stdout.py
+++ lib_record_stdout.py
@@ -29,8 +29,10 @@ et __exit__ (en sortie).
         self.stop_and_get()
         print("sortie par __exit__")
         
-    def __init__(self, filename='sortie.txt'):
+    def __init__(self, filename='sortie2.txt'):
         print("Initialisation réalisée dasn __init__")
+        import pdb
+        pdb.set_trace()
         self.filename=filename
         self.important = False
         self.memory = StringIO()
@@ -45,28 +47,34 @@ et __exit__ (en sortie).
         with open(self.filename, 'a') as f:
             for line in self.memory.read():
                 f.write(line)            
-        
+        print("SAVED in {}".format(self.filename))
     def stop_and_get(self):
         sys.stdout = self.old_stdout
         self.memory.seek(0)
         if self.important:
             self.save_memory_to_file()
 
+# Sur les décorateurs avec arguments, 
+# lire http://gillesfabio.com/blog/2010/12/16/python-et-les-decorateurs/
+def record_if_important(filename='toto.txt'):
+    """Enregistrement conditionnel de la sortie standard d'une fonction.
 
-
-def record_if_important(filename='output.txt'):
-    print("Je décore la fonction : '{}()'".format(funct.__name__))
-    def wrapper(*args, **kwargs): # indispensable pour récupérer les arguments
-                                  # de la fonction
-        print("Ceci est avant enregistrement")
-        print("Je vais exécuter la fonction")
-        a = list(args)
-        print("Liste des arguments :", a)
-        with  PersistentStdout(filename) as buf:
-              buf.important = funct(*args, **kwargs)
-        print("Ceci est après enregistrement")
-        return buf.important
-    return wrapper # et non pas wrapper()
+Enregistre la sortie standard si le résultat de la fonction est True.
+"""
+    def decorated(funct):
+        print("Je décore la fonction : '{}()'".format(funct.__name__))
+        def wrapper(*args, **kwargs): # indispensable pour récupérer les arguments
+                                      # de la fonction
+            print("Ceci est avant enregistrement")
+            print("Je vais exécuter la fonction")
+            a = list(args)
+            print("Liste des arguments :", a)
+            with  PersistentStdout(filename=filename) as buf:
+                  buf.important = funct(*args, **kwargs)
+            print("Ceci est après enregistrement")
+            return buf.important
+        return wrapper # et non pas wrapper()
+    return decorated   # ni decorated()
 
 @record_if_important(filename='tutu.txt')  
 def fonction_qui_ecrit_et_fait_des_tests(msg):
