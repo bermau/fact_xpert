d9ddceb4508744046b3d5acfd199b3bf95015d0a
MAUBERT Bertrand <U178211@dcher.local>
2017-05-06 13:56:03 +0200

    progrès sur extraction du csv

--- util_extracteur_data_from_csv.py
+++ util_extracteur_data_from_csv.py
@@ -3,16 +3,36 @@
 
 données au format csv separé par des |"""
 
- 
+# exmeples de http://pandas.pydata.org/pandas-docs/stable/10min.html
+
 import pandas as pd
 
+import matplotlib.pyplot as plt 
 
 
+from bm_u import title
+
+# importer les données et renommer les colonnes
 data = pd.read_csv('PRIVATE/extrait_mST69508_short.csv', sep='|',
                    encoding='ansi',
                    header=5)
 
+title("Importation brute")
 print(data.head(10))
+
+
+data.columns = ['Prescripteur', 'Objet', 'Dossier',
+                                     'CodeNABM', 'B', 'BHN', 'TB']
+
+title("Après modif des noms de colonnes")
+print(data.head(10))
+
+title("fontion describe")
+print(data.describe())
+
+title("Trier selon une colonne")
+print(data.sort_values(by = 'B'))
+
 
 if __name__ == "__main__":
     pass
