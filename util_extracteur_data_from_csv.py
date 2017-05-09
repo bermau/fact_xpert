0e2d0ff641cd5a76011e4400708b234208dbbb3b
MAUBERT Bertrand <U178211@dcher.local>
2017-05-09 18:22:58 +0200

    amélioration sur csv

--- util_extracteur_data_from_csv.py
+++ util_extracteur_data_from_csv.py
@@ -6,11 +6,16 @@ données au format csv separé par des |
 Quelques références au livre suivant :
 Analyse de données en Python/ Manipulation de données avec Pandas,
 Numpy et IPython. Wes McKinney, Ed Eyrolles, 2015.
+
 """
 
-# exmeples de http://pandas.pydata.org/pandas-docs/stable/10min.html
+
+# exemples de http://pandas.pydata.org/pandas-docs/stable/10min.html
+# importer les données du csv
+# exclure les données 
 
 import pandas as pd
+import numpy as np
 
 import matplotlib.pyplot as plt 
 
@@ -36,23 +41,28 @@ print(data.head(10))
 title("fontion describe")
 print(data.describe())
 
-title("Les lignes contenant Total")
+title("Les lignes contenant Total sont à exlure. ")
 print(data[data.Dossier=='Total'])
 title("et retirer ces lignes")
-data2 = data[data.Dossier != 'Total' and data.Prescripteur !="Total"]
-# data2 = data[data.Dossier != 'Total' and data.Prescripteur !="Total"]
 
+EXCLURE = "Total"
+# ci desous noter l'usage de & et non pas de and.
+data2 = data[(data.Dossier != EXCLURE) &
+             (data.Prescripteur != EXCLURE) &
+             (data.Objet != EXCLURE)]
 print(data2)
-
-
 
 title("Remplacer les tirets par des Na")# cf. McKinney p. 156
 # méthode avec query : data.query('line_race != 0').
+data3= data2.replace('-', np.nan)
+print(data3)
 
 title("Trier selon une colonne")
-print(data2.sort_values(by = 'B'))
+print(data3.sort_values(by = 'B'))
 
-
+DOSSIER = '17042664  18/04/2017'
+title("Selectionner les données du dossier "+ DOSSIER)
+print(data3[data3.Dossier==DOSSIER])
 
 
 if __name__ == "__main__":
